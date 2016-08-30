e924726a897efc12404ae93c0bb13c59687d29c4
Francois GRUCHALA <francois.gruchala@gmail.com>
2016-08-30 14:04:36 +0200

    FEAT(Filter) Size
    Add a filter to give size of an object or an array

---
+++ app/filters/size.filter.js
@@ -0,0 +1,42 @@
+/**
+ * Object Size 
+ * @namespace Filters
+ * @memberOf App
+ */
+(function () {
+    
+    'use strict';
+    
+    angular
+    .module('app.filters')
+    .filter('size', sizeFilter);
+    
+    /**
+     * @name sizeFilter
+     * @memberOf Filters
+     */
+    function sizeFilter () {
+        var filter = function(input) {
+            var size = 0;
+            
+            if(angular.isDefined(input)){   
+                if(angular.isArray(input)) {
+                    size = input.length;
+                }
+                else if(angular.isObject(input)) {
+                    angular.forEach(input, function(value, key) {
+                    size++; 
+                    });
+                }
+                else {
+                    throw new Error('Filter objectSize : input has to be an array or an object');
+                }
+            }
+            
+            return size;
+        }
+        
+        return filter;
+    }
+    
+})();
