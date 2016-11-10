397a5a3ed67e53c85c07afe614c56d2d7cbfc97b
Boris Couturier <couturier.boris@gmail.com>
2016-11-10 21:12:08 +0100

    FEAT (metrics)
    - Ajout de uptime
    - Ajout de httpSession

--- app/metrics/home/metrics-home.controller.js
+++ app/metrics/home/metrics-home.controller.js
@@ -1,52 +1,33 @@
 'use strict';
 
 (function () {
-    angular
-    .module('app.metrics')
-    .controller('MetricsHomeController', MetricsHomeController);
-    
-    MetricsHomeController.$inject = ['$rootScope', 'actuatorService'];
-    
-    function MetricsHomeController($rootScope, actuatorService) {
-        var vm = this;
+	angular
+		.module('app.metrics')
+		.controller('MetricsHomeController', MetricsHomeController);
 
-        vm.mem;
-		vm.memFree;
-		vm.heap;
-		vm.heapUsed;
+	MetricsHomeController.$inject = ['$rootScope', 'actuatorService'];
+
+	function MetricsHomeController($rootScope, actuatorService) {
+		var vm = this;
+
+		vm.mem = '';
+		vm.memFree = '';
+		vm.heap = '';
+		vm.heapUsed = '';
+		vm.httpSession = '';
+		vm.httpSessionMax = '';
 		vm.uptime = {};
 
 		vm.memPercent = memPercent;
 		vm.heapPercent = heapPercent;
 
-        activate();
+		activate();
 
 
 
-        function activate() {
-            getDatas();
+		function activate() {
+			getDatas();
 			$rootScope.$on('serviceUrlChange', getDatas);
-		}
-
-		function getDatas() {
-			actuatorService.metrics()
-				.then(function (metrics) {
-					vm.mem = parseInt(metrics.data['mem']) / 1024 | 0;
-					vm.memFree = parseInt(metrics.data['mem.free']) / 1024 | 0;
-					vm.heapUsed = parseInt(metrics.data['heap.used']) / 1024 | 0;
-					vm.heap = parseInt(metrics.data['heap']) / 1024 | 0;
-
-					var uptimeMoment = moment.duration(metrics.data['uptime']);
-					vm.uptime.days = uptimeMoment.asDays() | 0;
-					uptimeMoment.subtract(vm.uptime.days, 'd');
-					vm.uptime.hours = uptimeMoment.asHours() | 0;
-					uptimeMoment.subtract(vm.uptime.hours, 'h');
-					vm.uptime.minutes = uptimeMoment.asMinutes() | 0;
-					uptimeMoment.subtract(vm.uptime.minutes, 'm');
-					vm.uptime.second = uptimeMoment.asSeconds() | 0;
-
-					console.log(vm.uptime);
-				});
 		}
 
 		function memPercent() {
@@ -55,6 +36,43 @@
 
 		function heapPercent() {
 			return vm.heapUsed * 100 / vm.heap;
+		}
+
+		function getDatas() {
+			actuatorService.metrics()
+				.then(function (metrics) {
+					setMemData(metrics);
+					setHttpSessionData(metrics);
+					setUptimeData(metrics);
+				});
+		}
+
+		function setMemData(metrics) {
+			vm.mem = parseInt(metrics.data['mem']) / 1024 | 0;
+			vm.memFree = parseInt(metrics.data['mem.free']) / 1024 | 0;
+			vm.heapUsed = parseInt(metrics.data['heap.used']) / 1024 | 0;
+			vm.heap = parseInt(metrics.data['heap']) / 1024 | 0;
+		}
+
+		function setHttpSessionData(metrics) {
+			vm.httpSession = metrics.data['httpsessions.active'];
+			vm.httpSessionMax = metrics.data['httpsessions.max'];
+			if (vm.httpSessionMax === -1) {
+				vm.httpSessionMax = '∞';
+			}
+		}
+
+		function setUptimeData(metrics) {
+			var uptimeMoment = moment.duration(metrics.data['uptime']);
+			vm.uptime.days = uptimeMoment.asDays() | 0;
+			uptimeMoment.subtract(vm.uptime.days, 'd');
+			vm.uptime.hours = uptimeMoment.asHours() | 0;
+			uptimeMoment.subtract(vm.uptime.hours, 'h');
+			vm.uptime.minutes = uptimeMoment.asMinutes() | 0;
+			uptimeMoment.subtract(vm.uptime.minutes, 'm');
+			vm.uptime.second = uptimeMoment.asSeconds() | 0;
+
+			console.log(vm.uptime);
 		}
 	}
 })();
