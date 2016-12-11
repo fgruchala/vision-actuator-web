c2571a6aff5bb0ad1796acb64b43508f56d62c7d
Francois GRUCHALA <francois.gruchala@gmail.com>
2016-12-11 17:22:32 +0100

    FIX(Bean) Filters

--- app/beans/beans.controller.js
+++ app/beans/beans.controller.js
@@ -1,8 +1,3 @@
-/**
- * Controller of the beans page
- * @namespace Beans
- * @memberOf App
- */
 (function () {  
     'use strict';
     
@@ -12,20 +7,13 @@
     
     beansController.$inject = ['beansPrepData', 'scopesPrepData', '$mdDialog'];
     
-    /**
-     * @name beansController
-     * @param {Object} [beansPrepData] - Beans data 
-     * @param {Object} [scopesPrepData] - Filters data providing by beans data
-     * @param {@link https://material.angularjs.org/latest/api/service/$mdDialog | MaterialService} [$mdDialog]
-     * @memberOf Beans
-     */
     function beansController (beansPrepData, scopesPrepData, $mdDialog) {
         var vm = this;
-        var INC_ITEMS = 25;
+        var INC_ITEMS = 50;
         
         vm.beansPrepData = beansPrepData;
         vm.scopesPrepData = scopesPrepData;
-        vm.searchValue = {
+        vm.filtering = {
             bean: '',
             scopes: angular.copy(scopesPrepData)
         };
@@ -33,19 +21,12 @@
         
         vm.showFullInformations = showFullInformations;
         vm.more = more;
-        vm.search = search;        
-        vm.isSearch = isSearch;
-        vm.resetSearch = resetSearch;
+        vm.filter = filter;        
         vm.isSelectedScope = isSelectedScope;
         vm.selectScope = selectScope;
 
         
         
-        /**
-         * @name showFullInformations
-         * @param {Object} [bean] - Bean data to show
-         * @memberOf beansController
-         */
         function showFullInformations (bean) {
             $mdDialog.show({
                 controller: 'VisuBeanPopupController',
@@ -58,76 +39,27 @@
             });
         }
 
-        /**
-         * @name search
-         * @returns {boolean}
-         * @memberOf beansController
-         */
-        function search () {
+        function filter () {
             return function (beanDefinition) {
-                var searchValue = vm.searchValue.bean.toLowerCase();
-                var bean = beanDefinition.bean.toLowerCase();
-                var displayedByBean = true; 
-                var displayedByScope = false; 
-
-                if(searchValue !== '') {
-                    displayedByBean = bean.indexOf(searchValue) !== -1;
-                }
-
-                angular.forEach(vm.searchValue.scopes, function(scope, idx) {
-                    displayedByScope = displayedByScope || beanDefinition.scope.indexOf(scope) !== -1;
-                });
-
-                return displayedByBean && displayedByScope;
+                return beanDefinition.bean.toLowerCase().indexOf(vm.filtering.bean.toLowerCase()) !== -1 
+                    && vm.filtering.scopes.indexOf(beanDefinition.scope) !== -1;
             }
         }
 
-        /**
-         * @name isSearch
-         * @returns {boolean}
-         * @memberOf beansController
-         */
-        function isSearch () {
-            return vm.searchValue.bean !== '';
-        }
-
-        /**
-         * @name resetSearch
-         * @memberOf beansController
-         */
-        function resetSearch () {
-            vm.searchValue.bean = '';
-        }
-
-        /**
-         * @name more
-         * @memberOf beansController
-         */
         function more () {
             vm.limitValue += INC_ITEMS;
         }
 
-        /**
-         * @name isSelectedScope
-         * @params {String} [scope]
-         * @returns {boolean}
-         * @memberOf beansController
-         */
         function isSelectedScope (scope) {
-            return vm.searchValue.scopes.indexOf(scope) !== -1;
+            return vm.filtering.scopes.indexOf(scope) !== -1;
         }
 
-        /**
-         * @name selectScope
-         * @params {String} [scope]
-         * @memberOf beansController
-         */
         function selectScope (scope) {
             if(isSelectedScope(scope)) {
-                vm.searchValue.scopes.splice(vm.searchValue.scopes.indexOf(scope), 1);
+                vm.filtering.scopes.splice(vm.filtering.scopes.indexOf(scope), 1);
             }
             else {
-                vm.searchValue.scopes.push(scope);
+                vm.filtering.scopes.push(scope);
             }
         }
         
